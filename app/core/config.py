931ba8d0db0611a5e969d044c837efb9cdf05e5f
Thibault Bardinet-Langlois <langlois.bardinet.thibault@gmail.com>
2026-06-10 16:07:49 +0200

    feat(config): add settings management and database configuration

--- app/core/config.py
+++ app/core/config.py
@@ -5,10 +5,10 @@ class Settings(BaseSettings):
 
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
+    POSTGRES_HOST: str
     POSTGRES_PORT: int
+    POSTGRES_DB: str
     
-    DATABASE_URL: str
     UPLOAD_DIR: str
     BACKEND_CORS_ORIGINS: str
 
@@ -20,8 +20,18 @@ class Settings(BaseSettings):
     COOKIE_SAMESITE: str
     COOKIE_REFRESH_MAX_AGE: int
     
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+psycopg2://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+    
     class Config:
         env_file = ".env"
+        case_sensitive = True
+
         
         
 settings = Settings()
